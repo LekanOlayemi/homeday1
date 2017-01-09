af8f25ac0ace4393a62e71bcc611cde8e359ef56
lekanolayemi <lexzikan@gmail.com>
2017-01-09 09:33:44 +0100

    added librarygetPrimes.js

---
+++ app/librarygetPrimes.js
@@ -0,0 +1,12 @@
+module.exports = function(n) {
+    var p = []
+    for(var i = 2; i <=n; ++i)
+        if(getPrimes(i)) p.push(i)
+            return p
+}
+function getPrimes(n) {
+    for(var i = 2; i <= Math.sqrt(n); ++i)
+        if(n % i ==0)
+            return false
+        return true
+}
