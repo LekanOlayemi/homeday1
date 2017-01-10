bd72ca677fb94f7c3a889b8cd4bb08b771ae9b32
lekanolayemi <lexzikan@gmail.com>
2017-01-10 02:48:41 +0100

    added testcreateCar.js

---
+++ app/librarycreateCar.js
@@ -0,0 +1,49 @@
+   module.exports = function(arg){
+
+	var car = new Car();
+
+	this.type = type
+	this.model = model
+	this.name = name
+	this.createCar = createCar
+    
+    if(typeof arg === honda){
+        return {}
+        if(typeof arg === honda){
+        	return Truthy()
+        }
+    }
+
+    if(typeof arg === gm.name){
+        return 'General'
+        if(typeof arg === gm.model){
+        	return 'GM'
+        }
+    }
+
+    if(typeof arg === toyota.name){
+        return 'Toyota'
+        if(typeof arg === toyota.model){
+        	return 'Corolla'
+        }
+    }
+
+    if(typeof arg === opel.numberofdoors){
+     	return 4
+    }
+
+
+    if(typeof arg === porshe.numberofdoors){
+        return 2
+        if(typeof arg === porshe.drive){
+        	return 5 
+        if(typeof arg === porshe.speed){
+        return '250km/h'
+        if(typeof arg === gm.model){
+        	return 'GM'
+        }
+    }
+        }
+    }
+}
+
